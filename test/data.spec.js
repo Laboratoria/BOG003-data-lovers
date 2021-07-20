a5022d819a273169b856e4df7131e167f7aae4aa
Lorraine Gelis <lgelisdiaz@outlook.com>
2021-07-20 11:42:29 -0500

    Se terminan las pruebas unitarias con la cobertura solicitada

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,12 +3,12 @@ import { calculoEstadistico, ordenarFiltrar, filtrarTipo, filtrarRegion, ordenar
 import data from '../src/data/pokemon/pokemon.js';
 
 const entrada = [
-    { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } },
-    { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" } },
-    { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } },
-    { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" } },
-    { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" } },
-    { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" } }
+    { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+    { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" },
+    { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+    { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "62.308", baseProm: "123" },
+    { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" },
+    { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" }, baseDesviacion: "16.073", baseProm: "232" }
 ]
 
 const entradaDos = [
@@ -44,12 +44,31 @@ describe('ordenarFiltrar', () => {
         expect(typeof ordenarFiltrar).toBe('function');
     });
     const salidaOrdenarFiltrar = [
-        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" } }
+        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" },
+        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" }, baseDesviacion: "16.073", baseProm: "232" }
     ]
     it("deberia retornar los pokemon tipo fuego y ordenados por su numero", () => {
         expect(ordenarFiltrar(entrada, "fire", "num")).toEqual(salidaOrdenarFiltrar);
     })
+
+    const salidaOrdenarFiltrar2 = [
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" }
+    ]
+    it("deberia retornar los pokemon tipo poison ordenados de formas ascendente segun su respectiva desviación", () => {
+        expect(ordenarFiltrar(entrada, "poison", "baseDesviacion")).toEqual(salidaOrdenarFiltrar2);
+    })
+
+    const salidaOrdenarFiltrar3 = [
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" }
+    ]
+    it("deberia retornar los pokemon tipo grass ordenados de formas descendente segun su respectivo promedio", () => {
+        expect(ordenarFiltrar(entrada, "grass", "baseProm")).toEqual(salidaOrdenarFiltrar3);
+    })
+
+
 });
 
 describe("filtrarTipo", () => {
@@ -57,8 +76,8 @@ describe("filtrarTipo", () => {
         expect(typeof filtrarTipo).toBe('function')
     });
     const salidaTipo = [
-        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } }
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" }
     ]
     it("deberia retornar un array filtrado por tipo", () => {
         expect(filtrarTipo(entrada, "grass")).toEqual(salidaTipo);
@@ -70,11 +89,11 @@ describe('filtrarRegion', () => {
         expect(typeof filtrarRegion).toBe('function');
     });
     const salidaRegion = [
-        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" } }
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" },
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "62.308", baseProm: "123" },
+        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" }
     ]
     it("Deberia retornar los pokemon de kanto", () => {
         expect(filtrarRegion(entrada, "kanto")).toEqual(salidaRegion);
@@ -86,15 +105,63 @@ describe('ordenarPokemon', () => {
         expect(typeof ordenarPokemon).toBe('function');
     });
     const salidaOrdenar = [
-        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" } },
-        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" } },
-        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" } }
+        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "62.308", baseProm: "123" },
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" },
+        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" },
+        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" }, baseDesviacion: "16.073", baseProm: "232" },
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" }
     ]
-    it("Deberia retornar los pokemon ordenados alfabeticamente", () => {
+    it("Deberia retornar los pokemon ordenados alfabeticamente de forma ascendente", () => {
         expect(ordenarPokemon(entrada, "name", "asc")).toEqual(salidaOrdenar);
+    })
+
+    const salidaOrdenar2 = [
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" },
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "62.308", baseProm: "123" },
+        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" },
+        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" }, baseDesviacion: "16.073", baseProm: "232" }
+    ]
+    it("Deberia retornar los pokemon ordenados de forma ascendente por su número", () => {
+        expect(ordenarPokemon(entrada, "num", "asc")).toEqual(salidaOrdenar2);
+    })
+
+    const salidaOrdenar3 = [
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" },
+        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" }, baseDesviacion: "16.073", baseProm: "232" },
+        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" },
+        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "62.308", baseProm: "123" }
+    ]
+    it("Deberia retornar los pokemon ordenados de forma ascendente por su desviación", () => {
+        expect(ordenarPokemon(entrada, "baseDesviacion", "asc")).toEqual(salidaOrdenar3);
+    })
+
+    const salidaOrdenar4 = [
+        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" }, baseDesviacion: "16.073", baseProm: "232" },
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" },
+        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "62.308", baseProm: "123" },
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" }
+    ]
+    it("Deberia retornar los pokemon ordenados de forma descendente por su desviación", () => {
+        expect(ordenarPokemon(entrada, "baseProm", "desc")).toEqual(salidaOrdenar4);
+    })
+
+    const salidaOrdenar5 = [
+        { num: "003", name: "venusaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "4.933", baseProm: "192" },
+        { num: "006", name: "ho-oh", type: ["fire", "flying"], generation: { "num": "generation ii", "name": "johto" }, baseDesviacion: "16.073", baseProm: "232" },
+        { num: "005", name: "grimer", type: ["poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "50.083", baseProm: "138" },
+        { num: "002", name: "charmander", type: ["fire"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "13.892", baseProm: "109" },
+        { num: "001", name: "bulbasaur", type: ["grass", "poison"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "8.544", baseProm: "119" },
+        { num: "004", name: "abra", type: ["psychic"], generation: { "num": "generation i", "name": "kanto" }, baseDesviacion: "62.308", baseProm: "123" },
+    ]
+    it("Deberia retornar los pokemon ordenados alfabeticamente de forma descendente", () => {
+        expect(ordenarPokemon(entrada, "name", "desc")).toEqual(salidaOrdenar5);
     })
 
 });
