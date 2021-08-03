5422da35476a8ef0df0e3d362c49e348c4ef2378
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2021-08-03 18:37:24 -0500

    prueba insertar lammandola desde data.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+let allMovies = data.films;
+//console.log(allMovies[0].poster);
+let moviePoster=allMovies[2].poster;
+let screenPoster= document.createElement("IMG");
+screenPoster.setAttribute("src", moviePoster);
+screenPoster.setAttribute("alt", "third poster");
+
+document.body.appendChild(screenPoster);
+
+console.log(moviePoster);
+
+
+git 
+for (let item = 0; item < allMovies.length; item++) {
+    console.log(allMovies[item].title);
+    console.log()
+}
+ //innerhtml
+//createlement
+ //setatribute
+
+
+
+
+
+
+
+/* let imageObj = new Image();
+imageObj.src =allMovies.poster;
+//context.drawImage( imageObj, 0, 0 ); */
+
+//console.log(allMovies);
+
+/* let arrayData=data;
+console
+foreach(arrayData){
+
+} */
+
+//onsole.log(example, data);
+
